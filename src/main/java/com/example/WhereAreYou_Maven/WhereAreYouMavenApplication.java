72c83d216388eb4b6474303567b4e53e511c2aff
MajdiZouari <majdi.zouari@outlook.com>
2019-05-18 17:16:13 +0200

    Initial commit of full repository

---
+++ src/main/java/com/example/WhereAreYou_Maven/WhereAreYouMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.WhereAreYou_Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereAreYouMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereAreYouMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WhereAreYou_Maven/WhereAreYouMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.WhereAreYou_Maven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhereAreYouMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
